d0e68a827364ba11ab0639bc3e9d14b235dd0bc6
FitriAgustin <agustinftr21@gmail.com>
2025-09-09 07:43:16 +0700

    buat model yang nyambung ke konek

---
+++ model/dp_model.php
@@ -0,0 +1,27 @@
+<?php
+require_once "connect.php";
+
+class Model extends Connection{
+    function findAll(){
+        $sql = "SELECT * FROM products";
+        $result = $this->connect()->query;
+
+        $data = [];
+        if($result->num_rows> 0){
+            while($row = $result->fetch_assoc){
+                $data[] = $row;
+            }
+            return $data;
+        }
+    }
+
+    function findById($id){
+        $sql = "SELECT * FROM products WHERE id='$id";
+        $result = $this->connect()->query;
+        return $result->fetch_assoc;
+    }
+    
+}
+
+
+?>
