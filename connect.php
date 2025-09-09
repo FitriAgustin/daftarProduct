4a320f9a1f6f124f753f35248d8b19c7ac44e013
FitriAgustin <agustinftr21@gmail.com>
2025-09-09 07:34:45 +0700

    langkah awal membuat koneksi

--- connect.php
+++ connect.php
@@ -1,4 +1,16 @@
 <?php
+ class Connection{
+    private $h = "localhost";
+    private $u = "admin";
+    private $p = "admin123";
+    private $db = "daftarProduct";
+
+    function connect(){
+        return  new mysqli($this->h, $this->u, $this->p, $this->db);
+    }
+
+
+ }
 
 
 ?>
